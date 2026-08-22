2257ee7287adf9f6117bcff16c21e9d03fed853d
Rayber Reyes <rmreyesdonato@gmail.com>
2026-08-22 00:09:56 -0400

    fix: busqueda de pokemons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,8 +59,8 @@ export default function HomePage() {
 
       {isSearchActive && !searchLoading && !searchError && (
         <div className="pokemon-grid">
-          {displayPokemon.map((name) => (
-            <PokemonCard key={name} name={name} url={null} />
+          {displayPokemon.map((p) => (
+            <PokemonCard key={p.name} name={p.name} url={p.url} />
           ))}
         </div>
       )}
